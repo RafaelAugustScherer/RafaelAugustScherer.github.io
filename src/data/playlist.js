eeb1c579dcd9160794e1e05d3e6e70d7f427459d
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-03-26 15:09:33 -0300

    Add: Midnight Snack to playlist

--- src/data/playlist.js
+++ src/data/playlist.js
@@ -1,10 +1,12 @@
 import KingdomInBlue from './playlistData/Kingdom in Blue.mp3';
 import controlla from './playlistData/controlla.mp3';
 import stillbrething from './playlistData/still breathing.mp3';
+import MidnightSnack from './playlistData/Midnight Snack.mp3';
 
 import kingdomInBlueThumb from './playlistData/kingdom_in_blue.jpg';
 import controllaThumb from './playlistData/controlla.jpg';
 import stillbreathingThumb from './playlistData/still_breathing.jpg';
+import MidnightSnackThumb from './playlistData/midnight_snack.jpg';
 
 
 const sources = [
@@ -25,6 +27,12 @@ const sources = [
     artist: 'Kupla',
     src: stillbrething,
     thumb: stillbreathingThumb
+  },
+  {
+    name: 'Midnight Snack',
+    artist: 'Purple Cat',
+    src: MidnightSnack,
+    thumb: MidnightSnackThumb,
   }
 ];
 
