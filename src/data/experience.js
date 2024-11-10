21ce50b58df39a298154746c107f63db68d577d8
Rafael Scherer <rafaelaugustscherer@gmail.com>
2024-11-10 15:10:36 -0300

    Update: Add Gupy experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -4,6 +4,7 @@ import successEnglishIcon from '../assets/images/experience/academic/success-eng
 import kundenIcon from '../assets/images/experience/professional/kunden-icon.jpeg';
 import renovareIcon from '../assets/images/experience/professional/renovare-icon.jpeg';
 import madeiraMadeiraIcon from '../assets/images/experience/professional/madeiramadeira-icon.jpeg';
+import gupyIcon from '../assets/images/experience/professional/gupy-icon.jpeg'
 
 const academicExperience = [
   {
@@ -30,9 +31,15 @@ const academicExperience = [
 
 const professionalExperience = [
   {
+    name: 'Gupy',
+    tKey: 'professional.gupy',
+    date: '2024 - present',
+    icon: gupyIcon,
+  },
+  {
     name: 'MadeiraMadeira',
     tKey: 'professional.madeiraMadeira',
-    date: '2022 - present',
+    date: '2022 - 2024',
     icon: madeiraMadeiraIcon,
   },
   {
@@ -40,18 +47,13 @@ const professionalExperience = [
     tKey: 'professional.renovareTelecom',
     date: '2020 - 2021',
     icon: renovareIcon,
-    text: `Renovare Telecom is a \bTelecommunication Company\b from my hometown. The owners started the company in 2010 and \bafter 10 years of operation it is already the biggest in town.\b It also operates in many other countryside cities.
-    I was hired by the company in the middle of the pandemic as an \bIT Intern\b, some months after I'd been dismissed by Kunden. That opportunity was \bone of the best things that happened to me that year.\b
-    There I learned a lot about \benterprise servers, cloning and fixing corrupted operational systems, file recovery and some Debian commands and processes.\b I was responsible for the installation and setup of OSs for each and every employee. Upgrades and replacements were often needed as well. Most of the things I learned \bI owe to my colleagues, with years of experience in IT.\b`
   },
   {
     name: 'Kunden Systems',
     tKey: 'professional.kundenSystems',
     date: '2018 - 2020',
     icon: kundenIcon,
-    text: `Kunden Systems is an \bIndustrial & Comercial Enterprise Resource Planning (ERP) provider\b. It operates mainly in the \bsouth region of Brazil\b, but there are \bclients spread all over South America.\b
-    There I had \bmy first professional/working experience\b in the Information Technology area and it was really \bimportant for me and my perception of myself.\b My capabilites were put to test and I \blearned a lot with the people around me.\b From opening notebooks to applying fixes in \bPL/SQL Production and Test Databases.\b`
-  },
+  }
 ];
 
 export {
